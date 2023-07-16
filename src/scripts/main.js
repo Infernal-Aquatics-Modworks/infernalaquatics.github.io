7cca13744beb568defe5503d9b6212b908ce6591
NetherWarlord <nether.2.ather@gmail.com>
2023-07-16 01:37:07 -0400

    added some page elements
    Added page elements to the MCDJE page and the Modpacks page

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,5 +14,6 @@ const sr = ScrollReveal ({
 });
 
 sr.reveal('.hero-text', {delay:200, origin: 'top'});
-sr.reveal('.hero-img', {delay:400, origin: 'top'});
+sr.reveal('.hero-img', {delay:400, origin: 'right'});
+sr.reveal('.hero-packimg', {delay:400, origin: 'right'});
 sr.reveal('.icons', {delay:600, origin: 'left'});
