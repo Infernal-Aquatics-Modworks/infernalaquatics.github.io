d9ed726310a836dff2612d7895100c60b8a3a74d
NetherWarlord <nether.2.ather@gmail.com>
2023-07-19 23:54:15 -0400

    Added page elements
    Added the changelog, and mod list section to the mcdje pack page, along with fixing the vertical scroll issue with the carousel buttons.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -19,3 +19,6 @@ sr.reveal('.hero-packimg', {delay:400, origin: 'right'});
 sr.reveal('.hero-packs', {delay:400, origin: 'right'});
 sr.reveal('.icons', {delay:600, origin: 'left'});
 sr.reveal('.carousel-container', {delay:200, origin: 'left'});
+sr.reveal('.changelog-container', {delay:200, origin: 'right'});
+sr.reveal('.recognition-header', {delay:200, origin: 'left'});
+sr.reveal('.recognition-text', {delay:300, origin: 'left'});
